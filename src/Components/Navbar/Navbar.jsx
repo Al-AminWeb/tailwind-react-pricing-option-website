6fc74ed4c8d2587f6377d5e72335244db8089c68
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-13 16:23:47 +0600

    Toggle Menu Items and create pricing data

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,29 @@
 import React, {useState} from 'react';
 import {Menu} from "lucide-react";
-import { X } from 'lucide-react';
+import {X} from 'lucide-react';
 
-const navigationData = [
-    { id: 1, name: 'Home', path: '/' },
-    { id: 2, name: 'About', path: '/about' },
-    { id: 3, name: 'Services', path: '/services' },
-    { id: 4, name: 'Blog', path: '/blog' },
-    { id: 5, name: 'Contact', path: '/contact' },
-];
+const navigationData = [{id: 1, name: 'Home', path: '/'}, {id: 2, name: 'About', path: '/about'}, {
+    id: 3, name: 'Services', path: '/services'
+}, {id: 4, name: 'Blog', path: '/blog'}, {id: 5, name: 'Contact', path: '/contact'},];
 
 
 const Navbar = () => {
-    const [open,setOpen]= useState(false)
+    const [open, setOpen] = useState(false)
 
-    return (
-        <div>
-            <nav className="flex items-center justify-between mx-10">
-               <span className="flex" onClick={()=>setOpen(!open)}>
-        {open? <X></X> :  <Menu className="md:hidden"/>}
-
-                <h3 className="ml-4">My Navbar</h3>
-               </span>
-                <ul className="flex gap-2">
-                    {
-                        navigationData.map(route => <li><a href={route.path}>{route.name}</a></li>)
-                    }
-                </ul>
-
-                <button className="btn">Sign In</button>
-            </nav>
-            {/*<nav>*/}
-            {/*    <ul>*/}
-            {/*        <li><a href="/">Home</a></li>*/}
-            {/*        <li><a href="/about">About</a></li>*/}
-            {/*        <li><a href="/blog">Blog</a></li>*/}
-            {/*    </ul>*/}
-            {/*</nav>*/}
-        </div>
-    );
+    return (<div>
+        <nav className="flex items-center justify-between mx-10  text-black">
+               <span className="flex" onClick={() => setOpen(!open)}>
+            {open ? <X className='md:hidden'></X> : <Menu className="md:hidden"/>}
+                   <ul className={`md:hidden absolute
+             ${open ? 'top-8' : '-top-40'}
+             bg-amber-200`}>
+                {navigationData.map(route => <li><a href={route.path}>{route.name}</a></li>)}
+            </ul>
+            <h3 className="ml-4">My Navbar</h3>
+            </span>
+            <button className="btn">Sign In</button>
+        </nav>
+    </div>);
 };
 
 export default Navbar;
